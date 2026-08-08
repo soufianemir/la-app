7496e85eb6ed9c9434213368715858350958abec
soufianemir <soufiane.mir@gmail.com>
2026-08-08 11:44:39 +0200

    Refresh PWA cache for GPS fix

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = 'la-v1';
-const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+const CACHE = 'la-v2';
+const ASSETS = ['/', '/index.html', '/styles.css', '/geo-fix.js', '/app.js', '/manifest.webmanifest', '/icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(Promise.all([
+  caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+  self.clients.claim()
+])));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(fetch(event.request).then(response => {
